a679c283ed858b55549061a39ae9e97a921b1e03
Raphael Oh <raphaeloh96@gmail.com>
2022-11-14 20:34:58 -0800

    Updated ServiceWorker script path

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,8 +53,8 @@ function initializeServiceWorker() {
       try {
         // B3. TODO - Register './sw.js' as a service worker (The MDN article
         //            "Using Service Workers" will help you here)
-        await navigator.serviceWorker.register("/sw.js", {
-          scope: "/",
+        await navigator.serviceWorker.register("./sw.js", {
+          scope: "/Lab7_Starter/",
         });
         // B4. TODO - Once the service worker has been successfully registered, console
         //            log that it was successful.
